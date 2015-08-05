43b6844161f562efb18adeee029289a6c963b714
tutong <ttwoosta@gmail.com>
2015-08-05 09:53:50 -0400

    Student location model created and tested.

---
+++ OnTheMap/UDLocation.swift
@@ -0,0 +1,106 @@
+//
+//  UDLocation.swift
+//  OnTheMap
+//
+//  Created by Tu Tong on 8/5/15.
+//  Copyright (c) 2015 Tu Tong. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+public class UDLocation: NSManagedObject {
+    
+    struct JSONKeys {
+        static let createdAt: String = "createdAt"
+        static let firstName: String = "firstName"
+        static let lastName: String = "lastName"
+        static let latitude: String = "latitude"
+        static let longitude: String = "longitude"
+        static let mapString: String = "mapString"
+        static let mediaURL: String = "mediaURL"
+        static let objectId: String = "objectId"
+        static let uniqueKey: String = "uniqueKey"
+        static let updatedAt: String = "updatedAt"
+    }
+    
+    static let dateFormmater2 = NSDateFormatter()
+    public override class func initialize() {
+        dateFormmater2.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        super.initialize()
+    }
+    
+    public class func locationsFromResults(results: [[String: AnyObject]], moc: NSManagedObjectContext) -> [UDLocation] {
+        
+        // Location object entity
+        let entity = NSEntityDescription.entityForName("UDLocation", inManagedObjectContext: moc)!
+        
+        // returned locations array
+        var locations = [UDLocation]()
+        
+        for d in results {
+            var loc = UDLocation(entity: entity, insertIntoManagedObjectContext: moc)
+            loc.decodeWith(d)
+            locations.append(loc)
+        }
+        
+        return locations
+    }
+    
+    class func dateFormmater() -> NSDateFormatter {
+        struct Static {
+            static var onceToken: dispatch_once_t = 0
+            static var instance: NSDateFormatter? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = NSDateFormatter()
+            Static.instance?.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
+        }
+        return Static.instance!
+    }
+
+    @NSManaged public var objectId: String
+    @NSManaged public var uniqueKey: String
+    @NSManaged public var firstName: String
+    @NSManaged public var lastName: String
+    @NSManaged public var mapString: String
+    @NSManaged public var mediaURLString: String
+    @NSManaged public var latitude: NSNumber
+    @NSManaged public var longitude: NSNumber
+    @NSManaged public var createdAt: NSDate
+    @NSManaged public var updatedAt: NSDate
+    @NSManaged public var acl: String
+    
+    public var mediaURL: NSURL {
+        get {
+            return NSURL(string: mediaURLString)!
+        }
+    }
+    
+    public func decodeWith(dictionary: [String: AnyObject]) {
+        firstName = dictionary[JSONKeys.firstName] as! String
+        lastName = dictionary[JSONKeys.lastName] as! String
+        
+        if let lat = dictionary[JSONKeys.latitude] as? Float {
+            latitude = NSNumber(float: lat)
+        }
+        if let long = dictionary[JSONKeys.longitude] as? Float {
+            longitude = NSNumber(float: long)
+        }
+        
+        mapString = dictionary[JSONKeys.mapString] as! String
+        mediaURLString = dictionary[JSONKeys.mediaURL] as! String
+        objectId = dictionary[JSONKeys.objectId] as! String
+        uniqueKey = dictionary[JSONKeys.uniqueKey] as! String
+        
+        let dateFormmater = UDLocation.dateFormmater()
+        let dateFormmater1 = UDLocation.dateFormmater()
+        if let created = dictionary[JSONKeys.createdAt] as? String {
+            createdAt = dateFormmater.dateFromString(created)!
+        }
+        if let updated = dictionary[JSONKeys.updatedAt] as? String {
+            updatedAt = dateFormmater.dateFromString(updated)!
+        }
+    }
+
+}
--- OnTheMapTests/OnTheMapTests.swift
+++ OnTheMapTests/OnTheMapTests.swift
@@ -8,12 +8,26 @@
 
 import UIKit
 import XCTest
+import CoreData
+import OnTheMap
 
 class OnTheMapTests: XCTestCase {
     
+    var appDelegate: AppDelegate!
+    var moc: NSManagedObjectContext!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        // get app delegate from current application
+        appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        XCTAssertNotNil(appDelegate, "Cannot get application delegate")
+        
+        // managed object context
+        moc = appDelegate.managedObjectContext
+        XCTAssertNotNil(moc, "Managed object context cannot be nil")
+        
+        // clear context for every test
+        moc.reset()
     }
     
     override func tearDown() {
@@ -21,16 +35,110 @@ class OnTheMapTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measureBlock() {
-            // Put the code you want to measure the time of here.
+    func getStudentLocations() -> [String: AnyObject]! {
+        // // create bundle and get fixture
+        let bundle = NSBundle(forClass: self.dynamicType)
+        let fixtureURL = bundle.URLForResource("get-student-locations.json", withExtension: nil)
+        XCTAssertNotNil(fixtureURL)
+        
+        if let data = NSData(contentsOfURL: fixtureURL!) {
+            var error: NSError? = nil
+            if let locations = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &error) as? [String: AnyObject] {
+                return locations
+            }
+            else {
+                println(error)
+            }
         }
+        
+        return nil
     }
     
+    func createStudentLocationObject() -> UDLocation {
+        // entity
+        let udlentity = NSEntityDescription.entityForName("UDLocation", inManagedObjectContext: moc)
+        XCTAssertNotNil(udlentity)
+        
+        // student location object
+        var sl = UDLocation(entity: udlentity!, insertIntoManagedObjectContext: moc)
+        XCTAssertNotNil(sl)
+        XCTAssertTrue(sl.isKindOfClass(UDLocation))
+        
+        return sl
+    }
+    
+    func test_student_location() {
+        let fixture = getStudentLocations()
+        let results = fixture["results"] as! [[String: AnyObject]]
+        let firstLocation = results[0]
+        XCTAssertNotNil(firstLocation)
+        
+        var ust = createStudentLocationObject()
+        ust.decodeWith(firstLocation)
+        
+        XCTAssertEqual(ust.firstName, "Jarrod")
+        XCTAssertEqual(ust.lastName, "Parkes")
+        XCTAssertEqual(ust.mapString, "Huntsville, Alabama ")
+        XCTAssertEqual(ust.objectId, "JhOtcRkxsh")
+        XCTAssertEqual(ust.uniqueKey, "996618664")
+        
+        XCTAssertEqual(ust.mediaURLString, "https://www.linkedin.com/in/jarrodparkes")
+        XCTAssertEqual(ust.mediaURL.absoluteString!, "https://www.linkedin.com/in/jarrodparkes")
+        
+        XCTAssertEqual(ust.latitude.floatValue, 34.7303688)
+        XCTAssertEqual(ust.longitude.floatValue, -86.5861037)
+        
+        // too hard to compare dates
+        XCTAssertNotNil(ust.createdAt)
+        XCTAssertNotNil(ust.updatedAt)
+        
+    }
+    
+    func test_student_location_3rd_object() {
+        let fixture = getStudentLocations()
+        let results = fixture["results"] as! [[String: AnyObject]]
+        let firstLocation = results[2]
+        XCTAssertNotNil(firstLocation)
+        
+        var ust = createStudentLocationObject()
+        ust.decodeWith(firstLocation)
+        
+        XCTAssertEqual(ust.firstName, "Jason")
+        XCTAssertEqual(ust.lastName, "Schatz")
+        XCTAssertEqual(ust.mapString, "18th and Valencia, San Francisco, CA")
+        XCTAssertEqual(ust.objectId, "hiz0vOTmrL")
+        XCTAssertEqual(ust.uniqueKey, "2362758535")
+        
+        XCTAssertEqual(ust.mediaURLString, "http://en.wikipedia.org/wiki/Swift_%28programming_language%29")
+        XCTAssertEqual(ust.mediaURL.absoluteString!, "http://en.wikipedia.org/wiki/Swift_%28programming_language%29")
+        
+        XCTAssertEqual(ust.latitude.floatValue, 37.7617)
+        XCTAssertEqual(ust.longitude.floatValue, -122.4216)
+        
+        // too hard to compare dates
+        XCTAssertNotNil(ust.createdAt)
+        XCTAssertNotNil(ust.updatedAt)
+        
+    }
+    
+    
+    func test_student_locations_multiple() {
+        let fixture = getStudentLocations()
+        let results = fixture["results"] as! [[String: AnyObject]]
+        
+        
+        let locations = UDLocation.locationsFromResults(results, moc: moc)
+        
+        XCTAssertEqual(locations.count, 4)
+        
+        XCTAssertEqual(locations[0].uniqueKey, "996618664")
+        XCTAssertEqual(locations[1].uniqueKey, "872458750")
+        XCTAssertEqual(locations[2].uniqueKey, "2362758535")
+        XCTAssertEqual(locations[3].uniqueKey, "2256298598")
+        
+        
+        
+    }
+    
+        
 }
